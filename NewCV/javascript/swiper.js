cf4939f0c96ccb36fc2005515bb8aaa7b3606dda
Juan Carlos Lujan <juanclujan.92@gmail.com>
2023-01-17 11:18:57 -0300

    Add files via upload

---
+++ NewCV/javascript/swiper.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
